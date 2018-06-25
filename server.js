254496dd264eea9ece4a16f86fd5e5702004d78e
JannieP <jannie@pieterse.me>
2018-06-26 00:08:33 +1000

    trying to get around the mixed https -> http issue

--- server.js
+++ server.js
@@ -5,7 +5,15 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/AandGTestUI'));
+//app.use(express.static(__dirname + '/dist/AandGTestUI'));
+
+app.use(function (req, res, next){
+  if (req.headers['x-forwarded-proto'] === 'https') {
+    res.redirect('http://' + req.hostname + req.url);
+  } else {
+    next();
+  }
+});
 
 app.get('/*', function(req,res) {
   res.sendFile(path.join(__dirname+'/dist/AandGTestUI/index.html'));
